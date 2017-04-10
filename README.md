5765955737b8cc9c61326d188bdad4cf3ffea100
chenruixuan <www@chenruixuan.com>
2017-04-10 10:26:56 +0800

    readme file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# rsa
+# rsa for php
+
+
+thinks http://www.gaodaxian.com
+
+
+#generate key method
+
+openssl genrsa -aes256 -out private/ca.key.pem 2048
+
--- test/index.php
+++ test/index.php
@@ -8,10 +8,10 @@
 require __DIR__ . '/../autoload.php';
 
 
-$rsa=\Chenruixuan\Rsa\Rsa::getInstance();
+$rsa = \Chenruixuan\Rsa\Rsa::getInstance();
 //echo $rsa->RsaEncrypt("111");
 
-$str='zdpAAlYSmkoeIcc4tD86a3Ac/fUnCuE8XvaaYb3pBG38MKBF86Hk7F+hao2NPjdquuyKKgkAwrIcZzANAAlRW7cOMeD20pWjI4DUoUlB6ECAj4dhBKm4MNYjbrMLLrMbiArD6lvX8zeTMp9mxLOojmwfBbHESZw4CXIyKEpINf0=';
+$str = 'zdpAAlYSmkoeIcc4tD86a3Ac/fUnCuE8XvaaYb3pBG38MKBF86Hk7F+hao2NPjdquuyKKgkAwrIcZzANAAlRW7cOMeD20pWjI4DUoUlB6ECAj4dhBKm4MNYjbrMLLrMbiArD6lvX8zeTMp9mxLOojmwfBbHESZw4CXIyKEpINf0=';
 echo $rsa->RsaDecrypt($str);
 
 
